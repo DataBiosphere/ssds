a32a325513b3704783182b3fa4ebf9c3dbcfe7b0
Brian Hannafious <32105697+xbrianh@users.noreply.github.com>
2020-06-05 14:13:29 -0600

    Small changes to setup.py (#7)

--- setup.py
+++ setup.py
@@ -10,24 +10,24 @@ with open("README.md") as fh:
     long_description = fh.read()
 
 setup(
-    name='ssds',
-    version='0.0.0',
-    description='Simple data storage system for AWS and GCP.',
+    name="ssds",
+    version="0.0.0",
+    description="Simple data storage system for AWS and GCP.",
     long_description=long_description,
-    long_description_content_type='text/markdown',
-    url='https://github.com/xbrianh/ssds.git',
-    author='Brian Hannafious',
-    author_email='bhannafi@ucsc.edu',
-    license='MIT',
-    packages=find_packages(exclude=['tests']),
-    scripts=glob.glob('scripts/*'),
+    long_description_content_type="text/markdown",
+    url="https://github.com/xbrianh/ssds.git",
+    author="Brian Hannafious",
+    author_email="bhannafi@ucsc.edu",
+    license="MIT",
+    packages=find_packages(exclude=["tests"]),
+    scripts=glob.glob("scripts/*"),
     zip_safe=False,
     install_requires=install_requires,
-    platforms=['MacOS X', 'Posix'],
-    test_suite='tests',
+    platforms=["MacOS X", "Posix"],
+    test_suite="tests",
     classifiers=[
-        'Intended Audience :: Developers, Science/Research',
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 3.7'
+        "Intended Audience :: Developers, Science/Research",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.7"
     ]
 )
