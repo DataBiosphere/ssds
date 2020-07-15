2535ac3cb6503819e21d99c596b8dbd437314335
Brian Hannafious <32105697+xbrianh@users.noreply.github.com>
2020-07-15 15:55:50 -0600

    Fix versioning (#38)

--- setup.py
+++ setup.py
@@ -1,5 +1,7 @@
 import os
 import glob
+import warnings
+import subprocess
 from setuptools import setup, find_packages
 
 
@@ -9,9 +11,32 @@ install_requires = [line.rstrip() for line in open(os.path.join(os.path.dirname(
 with open("README.md") as fh:
     long_description = fh.read()
 
+def get_version():
+    filepath = os.path.join(os.path.dirname(__file__), "ssds", "version.py")
+    if os.path.isfile(filepath):
+        # In source distributions or builds, version is available in the generated ssds/version.py file
+        with open(filepath) as fh:
+            version = dict()
+            exec(fh.read().strip(), version)
+            return version['__version__']
+    else:
+        p = subprocess.run(["git", "describe", "--tags", "--match", "v*.*.*"], stdout=subprocess.PIPE)
+        if 128 == p.returncode:
+            warnings.warn('There are no git tags with version information. '
+                          'To tag the first commit as v0.0.0 use '
+                          '`git tag --annotate "v0.0.0" $(git rev-list --max-parents=0 HEAD) -m "v0.0.0"`')
+            return "0"
+        else:
+            p.check_returncode()
+            out = p.stdout.decode("ascii").strip()
+            if "-" in out:
+                out = out.split("-", 1)[0]
+            assert out.startswith("v")
+            return out[1:]
+
 setup(
     name="ssds",
-    version="0.0.0",
+    version=get_version(),
     description="Simple data storage system for AWS and GCP.",
     long_description=long_description,
     long_description_content_type="text/markdown",
