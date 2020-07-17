fd1b0776cf079a4ed6f0a8cc906d755edeb32954
Brian Hannafious <32105697+xbrianh@users.noreply.github.com>
2020-07-17 13:35:51 -0600

    Add "deployment" argument to staging CLI (#58)

--- ssds/cli/staging.py
+++ ssds/cli/staging.py
@@ -4,12 +4,13 @@ Upload, sync, and query staging area
 import os
 import argparse
 
-import ssds
 from ssds.deployment import Staging
 from ssds.cli import dispatch
 
 
-staging_cli = dispatch.group("staging", help=__doc__)
+staging_cli = dispatch.group("staging", help=__doc__, arguments={
+    "--deployment": dict(type=str, default=Staging.default.name, help="SSDS Deployment")
+})
 
 @staging_cli.command("upload", arguments={
     "--submission-id": dict(type=str, required=True, help="Submission id provided for your submission"),
@@ -21,26 +22,29 @@ def upload(args: argparse.Namespace):
     Upload a local directory tree to the staging bucket.
     Existing files in the submission will be overwritten.
     """
+    ssds = Staging[args.deployment].ssds
     root = os.path.abspath(os.path.normpath(args.path))
-    for ssds_key in Staging.default.ssds.upload(root, args.submission_id, args.name):
-        print(Staging.default.ssds.compose_blobstore_url(ssds_key))
+    for ssds_key in ssds.upload(root, args.submission_id, args.name):
+        print(ssds.compose_blobstore_url(ssds_key))
 
 @staging_cli.command("list")
 def list(args: argparse.Namespace):
     """
     List submissions in the staging bucket"
     """
-    for submission_id, submission_name in Staging.default.ssds.list():
+    ssds = Staging[args.deployment].ssds
+    for submission_id, submission_name in ssds.list():
         print(submission_id, submission_name)
 
 @staging_cli.command("list-submission", arguments={
     "--submission-id": dict(type=str, required=True, help="id of submission")
 })
 def list_submission(args: argparse.Namespace):
+    ssds = Staging[args.deployment].ssds
     submission_exists = False
-    for ssds_key in Staging.default.ssds.list_submission(args.submission_id):
+    for ssds_key in ssds.list_submission(args.submission_id):
         submission_exists = True
-        print(Staging.default.ssds.compose_blobstore_url(ssds_key))
+        print(ssds.compose_blobstore_url(ssds_key))
     if not submission_exists:
         print(f"No submission found for {args.submission_id}")
 
