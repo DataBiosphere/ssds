2f84d2ad77413a269c0adc42820e2a9522f0ebba
Brian Hannafious <32105697+xbrianh@users.noreply.github.com>
2020-08-26 14:35:27 -0600

    Refactor blobstore interface (#96)
    This is similar to the Python interfaces provided by S3 and GS.

--- ssds/blobstore/s3.py
+++ ssds/blobstore/s3.py
@@ -10,36 +10,48 @@ from gs_chunked_io.async_collections import AsyncSet
 import botocore.exceptions
 
 from ssds import aws
-from ssds.blobstore import BlobStore, AsyncPartIterator, Part, MultipartWriter, get_s3_multipart_chunk_size
+from ssds.blobstore import BlobStore, Blob, AsyncPartIterator, Part, MultipartWriter, get_s3_multipart_chunk_size
+
 
 class S3BlobStore(BlobStore):
     schema = "s3://"
 
-    def put_tags(self, bucket_name: str, key: str, tags: Dict[str, str]):
-        aws_tags = [dict(Key=key, Value=val)
-                    for key, val in tags.items()]
-        aws.client("s3").put_object_tagging(Bucket=bucket_name, Key=key, Tagging=dict(TagSet=aws_tags))
+    def __init__(self, bucket_name: str):
+        self.bucket_name = bucket_name
 
-    def get_tags(self, bucket_name: str, key: str) -> Dict[str, str]:
-        tagset = aws.client("s3").get_object_tagging(Bucket=bucket_name, Key=key)
+    def list(self, prefix=""):
+        for item in aws.resource("s3").Bucket(self.bucket_name).objects.filter(Prefix=prefix):
+            yield item.key
+
+    def blob(self, key: str) -> "S3Blob":
+        return S3Blob(self.bucket_name, key)
+
+class S3Blob(Blob):
+    def __init__(self, bucket_name: str, key: str):
+        self.bucket_name = bucket_name
+        self.key = key
+
+    def put_tags(self, tags: Dict[str, str]):
+        aws_tags = [dict(Key=k, Value=v)
+                    for k, v in tags.items()]
+        aws.client("s3").put_object_tagging(Bucket=self.bucket_name, Key=self.key, Tagging=dict(TagSet=aws_tags))
+
+    def get_tags(self) -> Dict[str, str]:
+        tagset = aws.client("s3").get_object_tagging(Bucket=self.bucket_name, Key=self.key)
         return {tag['Key']: tag['Value']
                 for tag in tagset['TagSet']}
 
-    def list(self, bucket: str, prefix="") -> Generator[str, None, None]:
-        for item in aws.resource("s3").Bucket(bucket).objects.filter(Prefix=prefix):
-            yield item.key
-
-    def get(self, bucket_name: str, key: str) -> bytes:
-        with closing(aws.resource("s3").Bucket(bucket_name).Object(key).get()['Body']) as fh:
+    def get(self) -> bytes:
+        with closing(aws.resource("s3").Bucket(self.bucket_name).Object(self.key).get()['Body']) as fh:
             return fh.read()
 
-    def put(self, bucket_name: str, key: str, data: bytes):
-        blob = aws.resource("s3").Bucket(bucket_name).Object(key)
+    def put(self, data: bytes):
+        blob = aws.resource("s3").Bucket(self.bucket_name).Object(self.key)
         blob.upload_fileobj(io.BytesIO(data))
 
-    def exists(self, bucket_name: str, key: str) -> bool:
+    def exists(self) -> bool:
         try:
-            self.size(bucket_name, key)
+            self.size()
             return True
         except botocore.exceptions.ClientError as e:
             if str(e.response['Error']['Code']) == str(requests.codes.not_found):
@@ -47,19 +59,19 @@ class S3BlobStore(BlobStore):
             else:
                 raise
 
-    def size(self, bucket_name: str, key: str) -> int:
-        blob = aws.resource("s3").Bucket(bucket_name).Object(key)
+    def size(self) -> int:
+        blob = aws.resource("s3").Bucket(self.bucket_name).Object(self.key)
         return blob.content_length
 
-    def cloud_native_checksum(self, bucket_name: str, key: str) -> str:
-        blob = aws.resource("s3").Bucket(bucket_name).Object(key)
+    def cloud_native_checksum(self) -> str:
+        blob = aws.resource("s3").Bucket(self.bucket_name).Object(self.key)
         return blob.e_tag.strip("\"")
 
-    def parts(self, bucket_name: str, key: str, threads: Optional[int]=None) -> "S3AsyncPartIterator":
-        return S3AsyncPartIterator(bucket_name, key, threads)
+    def parts(self, threads: Optional[int]=None) -> "S3AsyncPartIterator":
+        return S3AsyncPartIterator(self.bucket_name, self.key, threads)
 
-    def multipart_writer(self, bucket_name: str, key: str, threads: Optional[int]=None) -> "MultipartWriter":
-        return S3MultipartWriter(bucket_name, key, threads)
+    def multipart_writer(self, threads: Optional[int]=None) -> "MultipartWriter":
+        return S3MultipartWriter(self.bucket_name, self.key, threads)
 
 class S3AsyncPartIterator(AsyncPartIterator):
     def __init__(self, bucket_name, key, threads: Optional[int]=None):
