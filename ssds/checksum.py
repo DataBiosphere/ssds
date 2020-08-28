e90e314a278f4ba5eedf181ff2365ffba0c6a515
Brian Hannafious <bhannafi@ucsc.edu>
2020-08-27 23:29:48 -0700

    Permit crc32c instantiation with no data

--- ssds/checksum.py
+++ ssds/checksum.py
@@ -2,12 +2,16 @@ import base64
 import typing
 import binascii
 from hashlib import md5
+from typing import Optional
 
 import google_crc32c
 
 class crc32c:
-    def __init__(self, data: bytes=None):
-        self._checksum = google_crc32c.Checksum(data)
+    def __init__(self, data: Optional[bytes]=None):
+        if data is not None:
+            self._checksum = google_crc32c.Checksum(data)
+        else:
+            self._checksum = google_crc32c.Checksum(b"")
 
     def update(self, data: bytes):
         self._checksum.update(data)
