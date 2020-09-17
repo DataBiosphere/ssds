c168f6f8503c357e303bb966e0a05e3ba037c732
Brian Hannafious <32105697+xbrianh@users.noreply.github.com>
2020-09-17 14:14:45 -0600

    Raise BlobNotFoundError for local blobs (#129)

--- ssds/blobstore/local.py
+++ ssds/blobstore/local.py
@@ -1,10 +1,20 @@
 import os
 from math import ceil
+from functools import wraps
 from typing import Generator, Optional
 
 from ssds.blobstore import (BlobStore, Blob, AsyncPartIterator, Part, MultipartWriter, get_s3_multipart_chunk_size,
                             BlobNotFoundError, BlobStoreUnknownError)
 
+
+def catch_blob_not_found(func):
+    @wraps(func)
+    def wrapper(self, *args, **kwargs):
+        try:
+            return func(self, *args, **kwargs)
+        except FileNotFoundError as ex:
+            raise BlobNotFoundError(f"Could not find {self.key}") from ex
+    return wrapper
 
 class LocalBlobStore(BlobStore):
     def __init__(self, base_path: str):
@@ -23,10 +33,12 @@ class LocalBlob(Blob):
     def __init__(self, path: str):
         self.key = path
 
+    @catch_blob_not_found
     def get(self) -> bytes:
         with open(self.key, "rb") as fh:
             return fh.read()
 
+    @catch_blob_not_found
     def size(self) -> int:
         return os.path.getsize(self.key)
 
@@ -35,7 +47,10 @@ class LocalBlob(Blob):
 
 class LocalAsyncPartIterator:
     def __init__(self, key: str):
-        self.size = os.path.getsize(key)
+        try:
+            self.size = os.path.getsize(key)
+        except FileNotFoundError:
+            raise BlobNotFoundError(f"Could not find {key}")
         self.chunk_size = get_s3_multipart_chunk_size(self.size)
         self._number_of_parts = ceil(self.size / self.chunk_size) if 0 < self.size else 1
         self.handle = open(key, "rb")
