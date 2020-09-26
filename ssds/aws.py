a9b583c7c0e349c46ac2b5f2e218269f02cdd010
Brian Hannafious <bhannafi@ucsc.edu>
2020-09-26 12:20:33 -0700

    Increase number of http connections for aws

--- ssds/aws.py
+++ ssds/aws.py
@@ -3,15 +3,18 @@ from functools import lru_cache
 
 import boto3
 import botocore.session
-from botocore import credentials
+from botocore import config, credentials
+
+from ssds.concurrency import MAX_RPC_CONCURRENCY, MAX_PASSTHROUGH_CONCURRENCY
+
 
 @lru_cache()
 def resource(name):
-    return get_session().resource(name)
+    return get_session().resource(name, config=_boto_config())
 
 @lru_cache()
 def client(name):
-    return get_session().client(name)
+    return get_session().client(name, config=_boto_config())
 
 @lru_cache()
 def get_session():
@@ -25,3 +28,6 @@ def get_session():
     provider = session.get_component('credential_provider').get_provider('assume-role')
     provider.cache = credentials.JSONFileCache(working_dir)
     return boto3.Session(botocore_session=session)
+
+def _boto_config():
+    return config.Config(max_pool_connections=MAX_RPC_CONCURRENCY + MAX_PASSTHROUGH_CONCURRENCY)
