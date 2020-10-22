fd12c34f9d5234ea7e81146aea0bea0eb007070d
Brian Hannafious <bhannafi@ucsc.edu>
2020-10-22 13:32:12 -0700

    Add methods to retrieve cloud identities

--- ssds/gcp.py
+++ ssds/gcp.py
@@ -1,9 +1,12 @@
 import os
 import warnings
+import requests
 from functools import lru_cache
 from typing import Optional
 from requests.adapters import HTTPAdapter, DEFAULT_POOLSIZE
 
+import google.auth
+import google.auth.transport.requests
 from google.cloud.storage import Client
 
 from ssds.concurrency import MAX_RPC_CONCURRENCY, MAX_PASSTHROUGH_CONCURRENCY
@@ -31,3 +34,14 @@ def resolve_billing_project(billing_project: Optional[str]=None) -> Optional[str
         return os.environ['GCP_PROJECT']
     else:
         return None
+
+@lru_cache()
+def get_identity():
+    credentials, project_id = google.auth.default()
+    try:
+        credentials.refresh(google.auth.transport.requests.Request())
+        resp = requests.get(f"https://www.googleapis.com/oauth2/v1/tokeninfo?id_token={credentials.id_token}")
+        resp.raise_for_status()
+        return resp.json()['email']
+    except google.auth.exceptions.RefreshError:
+        return credentials.service_account_email
